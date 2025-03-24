728c31369c2a5fdf4c8a249cde507e24fcd524bc
HenrikasGrigaliunas <grigaliunas.h@gmail.com>
2025-03-24 16:57:19 +0200

    update

---
+++ exemples/google-ai/multimodal.py
@@ -0,0 +1,30 @@
+import os
+from google import genai
+from google.genai import types
+from rich import print
+
+from dotenv import load_dotenv
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(
+        api_key=gemini_api_key
+    )
+
+# Get the absolute path to the image
+image_path = os.path.join(os.path.dirname(__file__), "Image.jpg")
+
+# Read the image file
+with open(image_path, "rb") as f:
+    image = f.read()
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents=[
+        types.Part.from_bytes(data=image, mime_type="image/jpg"),
+        "Write a short and engaging blog post based on this picture.",
+    ],
+)
+
+print(response.text)
